898ccc57895f844e0128f7036d11df6596c9ca4f
Sourabh Kondapaka <sourabhkondapaka@gmail.com>
2017-06-01 16:54:17 +0530

    Finished w2 of c3

--- 3)AngularJS/w2/conFusion/app/scripts/app.js
+++ 3)AngularJS/w2/conFusion/app/scripts/app.js
@@ -80,4 +80,139 @@ app.controller('MenuController',['$scope',function($scope) {
     
     
 
-}]);
+}])
+
+    .controller('ContactController', ['$scope', function($scope) {
+
+        $scope.feedback = {mychannel:"", firstName:"", lastName:"",
+                           agree:false, email:"" };
+        
+        
+        var channels = [{value:"tel",label:"Tel."},
+                       {value:"Email",label:"Email"}];
+        
+        
+        $scope.channels = channels; 
+        $scope.invalidChannelSelection = false;
+    }])
+
+    .controller('FeedbackController', ['$scope', function($scope) {
+        
+        
+        $scope.sendFeedback =  function() {
+            
+            console.log($scope.feedback);
+            
+            if ($scope.feedback.agree && ($scope.feedback.mychannel === "")&& !$scope.feedback.mychannel) {
+                
+                $scope.invalidChannelSelection = true;
+                console.log('incorrect');
+            }
+            else {
+                $scope.invalidChannelSelection = false;
+                $scope.feedback = {mychannel:"", firstName:"", lastName:"",
+                                   agree:false, email:"" };
+                $scope.feedback.mychannel="";
+
+                $scope.feedbackForm.$setPristine();
+                console.log($scope.feedback);
+            } 
+            
+        };
+        
+
+    }])
+
+
+    .controller('DishDetailController', ['$scope', function($scope) {
+
+        var dish={
+            name:'Uthapizza',
+            image: 'images/uthapizza.png',
+            category: 'mains', 
+            label:'Hot',
+            price:'4.99',
+            description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
+            comments: [
+                {
+                    rating:5,
+                    comment:"Imagine all the eatables, living in conFusion!",
+                    author:"John Lemon",
+                    date:"2012-10-16T17:57:28.556094Z"
+                },
+                {
+                    rating:4,
+                    comment:"Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
+                    author:"Paul McVites",
+                    date:"2014-09-05T17:57:28.556094Z"
+                },
+                {
+                    rating:3,
+                    comment:"Eat it, just eat it!",
+                    author:"Michael Jaikishan",
+                    date:"2015-02-13T17:57:28.556094Z"
+                },
+                {
+                    rating:4,
+                    comment:"Ultimate, Reaching for the stars!",
+                    author:"Ringo Starry",
+                    date:"2013-12-02T17:57:28.556094Z"
+                },
+                {
+                    rating:2,
+                    comment:"It's your birthday, we're gonna party!",
+                    author:"25 Cent",
+                    date:"2011-12-02T17:57:28.556094Z"
+                }
+
+            ]
+        };
+
+        $scope.dish = dish;
+        $scope.feedback = {yourName:"",rating:5, yourComments:"",curDate:"" };
+        $scope.feedback.curDate = new Date();
+        
+
+
+
+    }])
+
+    .controller('DishCommentController', ['$scope', function($scope) {
+
+        //Step 1: Create a JavaScript object to hold the comment from the form
+   
+                
+        
+        
+
+        $scope.submitComment = function () {
+
+            $scope.user = {rating:$scope.feedback.rating,
+                           comment:$scope.feedback.yourComments,
+                           author:$scope.feedback.yourName,
+                           date:$scope.feedback.curDate            
+                          };
+
+            $scope.dish.comments.push($scope.user);
+            
+            
+            
+            
+            console.log($scope.user);
+            
+            console.log($scope.dish);
+            
+            $scope.commentForm.$setPristine();  //not working
+            $scope.commentForm.$setUntouched();  //not working
+            
+            $scope.feedback = {yourName:"",rating:5, yourComments:"",curDate:"" };
+            
+            
+
+        };
+    }])
+
+
+
+
+;
